10fd168d7b8d75bff73485d441d7818e11bb04bb
Leon Davis <leon.davis@lad-tech.com>
2015-09-13 20:21:42 +0100

    Bump version and tidy files

--- spec/page_magic/drivers/rack_test_spec.rb
+++ spec/page_magic/drivers/rack_test_spec.rb
@@ -3,7 +3,8 @@ module PageMagic
   class Drivers
     describe RackTest do
       it "is capybara's rack test driver" do
-        expect(described_class.build(:app, browser: :rack_test, options: {})).to be_a(Capybara::RackTest::Driver)
+        driver = described_class.build(:app, browser: :rack_test, options: {})
+        expect(driver).to be_a(Capybara::RackTest::Driver)
       end
     end
   end
--- spec/page_magic/usage/defining_page_elements_spec.rb
+++
@@ -1,6 +0,0 @@
-describe 'The Elements of a Page' do
-  include Capybara::DSL
-
-  describe 'a Page Class' do
-  end
-end
