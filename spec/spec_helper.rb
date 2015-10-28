62d1939256b64c1aa57cd4d1b0bf650fd77d0f9b
Leon Davis <leon.davis@lad-tech.com>
2015-10-28 18:06:40 +0000

    merge circle ci changes from master

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,12 @@
 Bundler.require
 $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
 
+require 'support/shared_contexts'
+
 if ENV['coverage']
   require "codeclimate-test-reporter"
   require 'simplecov'
   CodeClimate::TestReporter.start
 end
-
-require 'page_magic'
-require 'capybara/rspec'
-require 'helpers/capybara'
 
 require 'page_magic'
