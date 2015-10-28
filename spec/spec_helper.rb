4b07a9511469b1991ae36b9c335db9fca1ab4f62
Leon Davis <leon.davis@lad-tech.com>
2015-10-28 21:31:48 +0000

    adding pull review hooks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
 require 'support/shared_contexts'
 
 if ENV['coverage']
-  require "codeclimate-test-reporter"
+  # require "codeclimate-test-reporter"
   require 'simplecov'
   CodeClimate::TestReporter.start
 end
