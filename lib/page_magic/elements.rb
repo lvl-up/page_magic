2db8a7af5bb12c59aa5bbc638d8551c2d6052780
Leon Davis <leon.davis@lad-tech.com>
2015-11-02 19:49:26 +0000

    Adding documentation to element method

--- lib/page_magic/elements.rb
+++ lib/page_magic/elements.rb
@@ -3,6 +3,7 @@ module PageMagic
   # module Elements - contains methods that add element definitions to the objects it is mixed in to
   module Elements
     INVALID_METHOD_NAME_MSG = 'a method already exists with this method name'
+    TYPES = [:text_field, :button, :link, :checkbox, :select_list, :radios, :textarea]
 
     class << self
       def included(clazz)
@@ -21,6 +22,34 @@ module PageMagic
       element_definitions.values.collect { |definition| definition.call(browser_element, *args) }
     end
 
+    # Creates an element defintion.
+    # Element defintions contain specifications for locating them and other sub elements.
+    # if a block is specified then it will be executed against the element defintion.
+    # This method is aliased to each of the names specified in {TYPES TYPES}
+    # @example
+    #   element :widget, id: 'widget' do
+    #     link :next, text: 'next'
+    #   end
+    # @overload element(name, selector, &block)
+    #  @param [Symbol] name the name of the element.
+    #  @param [Hash] selector a key value pair defining the method for locating this element
+    #  @option selector [String] :text text contained within the element
+    #  @option selector [String] :css css selector
+    #  @option selector [String] :id the id of the element
+    #  @option selector [String] :name the value of the name attribute belonging to the element
+    #  @option selector [String] :label value of the label tied to the require field
+    # @overload element(element_class, &block)
+    #  @param [ElementClass] element_class a custom class of element that inherits {Element}.
+    #   the name of the element is derived from the class name. the Class name coverted to snakecase.
+    #   The selector must be defined on the class itself.
+    # @overload element(name, element_class, &block)
+    #  @param [Symbol] name the name of the element.
+    #  @param [ElementClass] element_class a custom class of element that inherits {Element}.
+    #   The selector must be defined on the class itself.
+    # @overload element(name, element_class, selector, &block)
+    #  @param [Symbol] name the name of the element.
+    #  @param [ElementClass] element_class a custom class of element that inherits {Element}.
+    #  @param [Hash] selector a key value pair defining the method for locating this element. See above for details
     def element(*args, &block)
       block ||= proc {}
 
@@ -35,8 +64,6 @@ module PageMagic
         section_class.new(name, parent_browser_element, options).expand(*e_args, &block)
       end
     end
-
-    TYPES = [:text_field, :button, :link, :checkbox, :select_list, :radios, :textarea]
 
     TYPES.each { |type| alias_method type, :element }
 
