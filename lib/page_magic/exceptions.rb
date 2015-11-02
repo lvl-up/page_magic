d696d035611cab808a013c6ef9dd79a5c34e9fcc
Leon Davis <leon.davis@lad-tech.com>
2015-11-02 00:25:55 +0000

    disabling some of the pull review checks because it is asking for documentation on constants that are sensibly named already

--- lib/page_magic/exceptions.rb
+++ lib/page_magic/exceptions.rb
@@ -1,29 +1,22 @@
 module PageMagic
-  # class UnsupportedSelectorException - raised when a user has used a selector not supported by page magic
   class UnsupportedSelectorException < Exception
   end
 
-  # class UndefinedSelectorException - raised when a PageElement has been defined without a selector
   class UndefinedSelectorException < Exception
   end
 
-  # class InvalidElementNameException - rasied when name used is already by another element or method.
   class InvalidElementNameException < Exception
   end
 
-  # class InvalidMethodNameException - raised when a page element name with the same name already exists
   class InvalidMethodNameException < Exception
   end
 
-  # class UnsupportedCriteriaException - raised when unsupported selection criteria are used.
   class UnsupportedCriteriaException < Exception
   end
 
-  # class ElementMissingException - raised when a element with given name can not be found.
   class ElementMissingException < Exception
   end
 
-  # class InvalidURLException - raised if url is poorly formated or not supplied
   class InvalidURLException < Exception
   end
 
