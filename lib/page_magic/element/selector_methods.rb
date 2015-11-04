68587a385b4ff3f753e5fade91b5e246497163e0
Leon Davis <leon.davis@lad-tech.com>
2015-11-04 23:23:13 +0000

    adding documentation

--- lib/page_magic/element/selector_methods.rb
+++ lib/page_magic/element/selector_methods.rb
@@ -2,6 +2,8 @@ module PageMagic
   class Element
     # module SelectorMethods - adds method for getting and setting an element selector
     module SelectorMethods
+      # Gets/Sets a selector
+      # @param [Hash] selector method for locating the browser element. E.g. text: 'the text'
       def selector(selector = nil)
         return @selector unless selector
         @selector = selector
