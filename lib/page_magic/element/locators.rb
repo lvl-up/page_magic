99f613b7ce061c7b45b7eb9cb69eef5f15cb056e
Leon Davis <leon.davis@lad-tech.com>
2015-11-11 23:30:25 +0000

    fix pull review messages

--- lib/page_magic/element/locators.rb
+++ lib/page_magic/element/locators.rb
@@ -2,11 +2,13 @@ module PageMagic
   class Element
     # contains method for finding element definitions
     module Locators
+      # message used when raising {ElementMissingException} from methods within this module
       ELEMENT_MISSING_MSG = 'Could not find: %s'
 
       # find an element definition based on its name
       # @param [Symbol] name name of the element
       # @return [Element] element definition with the given name
+      # @raise [ElementMissingException] raised when element with the given name is not found
       def element_by_name(name)
         defintion = element_definitions[name]
         fail ElementMissingException, (ELEMENT_MISSING_MSG % name) unless defintion
