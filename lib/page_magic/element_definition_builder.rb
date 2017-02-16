37e2d268a21138ffa3e842d0e4b738d36164fd12
leon <leon_a_d@yahoo.com>
2017-02-16 23:54:00 +0000

    tidy up

--- lib/page_magic/element_definition_builder.rb
+++ lib/page_magic/element_definition_builder.rb
@@ -4,7 +4,7 @@ module PageMagic
     INVALID_SELECTOR_MSG = 'Pass a locator/define one on the class'.freeze
     attr_reader :definition_class, :options, :selector, :type, :element, :query_builder
 
-    def initialize(definition_class:, selector:, type:, options: { multiple_results: false }, element: nil)
+    def initialize(definition_class:, selector:, type:, options: {}, element: nil)
       unless element
         selector ||= definition_class.selector
         raise UndefinedSelectorException, INVALID_SELECTOR_MSG if selector.nil? || selector.empty?
@@ -15,13 +15,13 @@ module PageMagic
       @type = type
       @query_builder = Element::QueryBuilder.find(type)
 
-      @options = options
+      @options = { multiple_results: false }.merge(options)
       @element = element
     end
 
     # @return [Capybara::Query] query to find this element in the browser
     def build_query
-      multiple_results = options.delete(:multiple_results) || false
+      multiple_results = options.delete(:multiple_results)
       query_builder.build(selector, options, multiple_results: multiple_results)
     end
 
