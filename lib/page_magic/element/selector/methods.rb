3b94a4e29b070f8df421e39fb108030be3e4d642
Leon Davis <leon.davis@lvl-up.uk>
2021-06-14 22:24:55 +0100

    Reorg selector components

---
+++ lib/page_magic/element/selector/methods.rb
@@ -0,0 +1,16 @@
+module PageMagic
+  class Element
+    class Selector
+      # module SelectorMethods - adds method for getting and setting an element selector
+      module Methods
+        # Gets/Sets a selector
+        # @param [Hash<symbol,string>] selector method for locating the browser element. E.g. text: 'the text'
+        def selector(selector = nil)
+          return @selector unless selector
+
+          @selector = selector
+        end
+      end
+    end
+  end
+end
