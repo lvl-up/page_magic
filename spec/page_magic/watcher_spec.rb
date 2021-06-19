488c09d1e6cd636f9d69192352839253c6c43bd7
Leon Davis <leon.davis@lvl-up.uk>
2021-06-19 19:14:37 +0100

    Tidy up watcher spec
    fix `rubocop-rspec` complaints

--- spec/page_magic/watcher_spec.rb
+++ spec/page_magic/watcher_spec.rb
@@ -2,62 +2,50 @@
 
 RSpec.describe PageMagic::Watcher do
   describe '#initialize' do
-    context 'check has not been called yet' do
-      subject do
-        described_class.new(:custom_watcher)
-      end
-
-      it 'sets last to nil' do
-        expect(subject.observed_value).to be_nil
+    context 'when `check` has not been called yet' do
+      it 'sets `observed_value` to nil' do
+        instance = described_class.new(:custom_watcher)
+        expect(instance.observed_value).to be_nil
       end
     end
   end
 
   describe '#check' do
-    context 'method supplied to constructor' do
-      subject do
-        described_class.new(:object_id)
-      end
-
+    context 'when a method supplied to constructor' do
       it 'assigns last to be the result of calling the method' do
-        subject.check(self)
-        expect(subject.observed_value).to eq(object_id)
+        instance = described_class.new(:object_id)
+        instance.check(self)
+        expect(instance.observed_value).to eq(object_id)
       end
     end
 
-    context 'name and attribute supplied to constructor' do
-      subject do
-        described_class.new(:my_button, :text)
-      end
-
-      it 'assigns last to the value of attribute definined in the constructor' do
-        browser_element = double(text: :hello)
-        page_element = double(my_button: browser_element)
-        expect(subject.check(page_element).observed_value).to eq(:hello)
+    context 'when name and attribute supplied to constructor' do
+      it 'assigns `observed_value` to the value of the attribute' do
+        instance = described_class.new(:my_button, :text)
+        browser_element = instance_double(Capybara::Node::Element, text: :hello)
+        page_element = Struct.new(:my_button).new(browser_element)
+        expect(instance.check(page_element).observed_value).to eq(:hello)
       end
     end
 
-    context 'block supplied to constructor' do
-      def method_on_self(value = nil)
-        return @value unless value
-
-        @value = value
-      end
-
-      subject do
+    context 'when a block is supplied' do
+      subject(:instance) do
         described_class.new(:custom_watcher) do
-          method_on_self(:called)
+          method_call
           :result
         end
       end
 
-      it 'is called on self' do
-        subject.check(self)
-        expect(method_on_self).to be(:called)
+      it 'is executed in the context the caller' do
+        context = Struct.new(:method_call).new(nil)
+        allow(context).to receive(:method_call)
+        instance.check(context)
+        expect(context).to have_received(:method_call)
       end
 
-      it 'assigns last to the resut of the block' do
-        expect(subject.check(self).observed_value).to eq(:result)
+      it 'assigns last to the result of the block' do
+        context = Struct.new(:method_call).new(nil)
+        expect(instance.check(context).observed_value).to eq(:result)
       end
     end
   end
