4ea634b51f9a9df10b2aadad3a9ea3acbeb46fd7
Leon Davis <leon.davis@lvl-up.uk>
2021-06-23 20:19:03 +0100

    Fix rubocop errors

--- spec/page_magic/utils/string_spec.rb
+++ spec/page_magic/utils/string_spec.rb
@@ -2,27 +2,25 @@
 
 RSpec.describe PageMagic::Utils::String do
   describe '.classify' do
-    context 'parameter is symbol' do
+    context 'when parameter is symbol' do
       it 'returns a string' do
         expect(described_class.classify(:Symbol)).to eq('Symbol')
       end
     end
 
-    context 'parameter is string' do
+    context 'when parameter is string' do
       it 'returns a string' do
         expect(described_class.classify(:String)).to eq('String')
       end
     end
 
-    context 'single word' do
-      context 'first letter is lower case' do
-        it 'converts the first letter to uppercase' do
-          expect(described_class.classify(:symbol)).to eq('Symbol')
-        end
+    context 'when the first letter is lower case' do
+      it 'converts the first letter to uppercase' do
+        expect(described_class.classify(:symbol)).to eq('Symbol')
       end
     end
 
-    context 'snakecase' do
+    context 'when parameter is in snakecase' do
       it 'removes underscores and capitalises each word' do
         expect(described_class.classify(:snake_case)).to eq('SnakeCase')
       end
--- spec/page_magic/wait_methods_spec.rb
+++ spec/page_magic/wait_methods_spec.rb
@@ -1,41 +1,35 @@
 # frozen_string_literal: true
 
 RSpec.describe PageMagic::WaitMethods do
-  subject do
-    Object.new.tap do |o|
-      o.extend(described_class)
+  include described_class
+
+  describe '#wait_until' do
+    let(:default_options) { { timeout_after: 0.1, retry_every: 0.05 } }
+    let!(:start_time) { Time.now }
+
+    it 'waits until the prescribed thing has happened' do
+      expect { wait_until(**default_options) { true } }.not_to raise_exception
     end
-  end
 
-  let(:default_options) { { timeout_after: 0.1, retry_every: 0.05 } }
-
-  it 'waits until the prescribed thing has happened' do
-    expect { subject.wait_until(**default_options) { true } }.not_to raise_exception
-  end
-
-  it 'keeps trying for a specified period' do
-    start_time = Time.now
-
-    expect { subject.wait_until(**default_options) { false } }.to raise_exception PageMagic::TimeoutException
-
-    expect(Time.now - default_options[:timeout_after]).to be > start_time
-  end
-
-  context 'timeout_after specified' do
-    it 'throws an exception if when the prescribed action does not happen in time' do
-      expect { subject.wait_until(**default_options) { false } }.to raise_error PageMagic::TimeoutException
+    it 'keeps trying for a specified period' do
+      wait_until(**default_options) { false }
+    rescue PageMagic::TimeoutException
+      expect(Time.now - default_options[:timeout_after]).to be > start_time
     end
-  end
 
-  context 'retry time specified' do
-    it 'retries at the given interval' do
-      count = 0
-      expect do
-        subject.wait_until(timeout_after: default_options[:timeout_after] * 2, retry_every: 0.1) do
-          count += 1
-        end
-      end.to raise_exception PageMagic::TimeoutException
-      expect(count).to eq(2)
+    context 'when `timeout_after` specified' do
+      it 'throws an exception if when the prescribed action does not happen in time' do
+        expect { wait_until(**default_options) { false } }.to raise_error PageMagic::TimeoutException
+      end
+    end
+
+    context 'when retry time specified' do
+      it 'retries at the given interval' do
+        count = 0
+        expect { wait_until(timeout_after: 0.2, retry_every: 0.1) { count += 1 } }
+          .to raise_exception(PageMagic::TimeoutException)
+          .and change { count }.by(2)
+      end
     end
   end
 end
